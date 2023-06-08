ff7f5ae034cfa21fd0c1c7e16827fe6392010597
William Roberson <billkeyshawn03@gmail.com>
2023-06-08 15:18:41 -0400

    changes cities

---
+++ mini-project/cities.js
@@ -0,0 +1,6 @@
+const citiesArray = [
+  { City: "Benbrook, Tx", Latitude: 32.6732, Longitude: -97.4606 },
+  { City: "Pittsburgh, Pa", Latitude: 40.4406, Longitude: 79.9959 },
+  { City: "Miami, Fl", Latitude: 25.7617, Longitude: 80.192 },
+  { City: "Berekley, CA", Latitude: 37.8728324788388, Longitude:-122.27235076525416 },
+];
---
+++ mini-project/main.js
@@ -0,0 +1,29 @@
+const citiesArray = document.querySelector("#city-ddl");
+ 
+
+function loadCitiesArray() {
+    let option = new Option("Select City...", "");
+    citiesArray.appendChild(option);
+   
+    for (const city of citiesArray) {
+      let option = document.createElement("option");
+      option.value = city.name;
+      option.innerText = city.name;
+      citiesArray.appendChild(option);
+    }
+  }
+  loadCitiesArray();
+
+  fetch(`https://api.weather.gov/points/${city.latitude} ${city.longitude}`)
+        .then((response) => response.json())
+        .then((point) =>  {
+            
+           console.log(properites.forecast);
+        });
+
+// Test API using REST Client 
+// Call the Points API with Lat, Long
+// Then log out the response data: office grid x,y
+// Then call the forecast API with grid x,y + gridID
+// Then log out the forecast data 
+// Then add to table 
